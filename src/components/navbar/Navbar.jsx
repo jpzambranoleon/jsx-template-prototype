87ab3f4684f98ea470f8378db4819e6c47c81b93
Jean-Paul <jpzambrano.leon@gmail.com>
2021-12-24 14:48:11 -0600

    updated Navbar.jsx

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import "./navbar.css"
+
+function Navbar() {
+    return (
+        <div className="navbar">
+            <div className="searchbar">
+                <input type="search" placeholder="Search for creators, inspirations, and projects" />
+            </div>
+        </div>
+    )
+}
+
+export default Navbar;
