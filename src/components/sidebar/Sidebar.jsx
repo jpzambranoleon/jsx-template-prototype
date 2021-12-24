60e2d5a2982a7b660b8c2a11ba46b6d120f2a709
Jean-Paul <jpzambrano.leon@gmail.com>
2021-12-24 02:09:04 -0600

    updated files

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -15,66 +15,64 @@ function Sidebar() {
         <div className="sidebar">
             <div className="logo-content">
                 <div className="logo">
-                    <div className="logo-name">someSocial</div>
                 </div>
                 <MenuIcon className="menu-icon"/>
             </div>
+            <a className="profile">
+                <div className="profile-photo">
+                    <img src="/assets/person/diana.jpg"/>
+                </div>
+                <div className="handle">
+                    <h4>Diana Ayi</h4>
+                    <p className="text-muted">
+                        @dayi
+                    </p>
+                </div>
+            </a>
             <ul className="nav-list">
                 <li>
-                    <a href="menu-item active">
+                    <a className="menu-item active" href="#">
                         <i><DashboardIcon htmlColor="purple" /></i>
                         <span className="links-name">Dashboard</span>
                     </a>
                 </li>
                 <li>
-                    <a href="#">
+                    <a className="menu-item" href="#">
                         <i><PersonIcon htmlColor="goldenrod"/></i>
                         <span className="links-name">User</span>
                     </a>
                 </li>
                 <li>
-                    <a href="#">
+                    <a className="menu-item" href="#">
                         <i><ChatIcon htmlColor='green' /></i>
                         <span className="links-name">Messages</span>
                     </a>
                 </li>
                 <li>
-                    <a href="#">
+                    <a className="menu-item" href="#">
                         <i><AnalyticsIcon htmlColor='tomato'/></i>
                         <span className="links-name">Analytics</span>
                     </a>
                 </li>
                 <li>
-                    <a href="#">
+                    <a className="menu-item" href="#">
                         <i><ColorLensIcon htmlColor='pink' /></i>
                         <span className="links-name">Theme</span>
                     </a>
                 </li>
                 <li>
-                    <a href="#">
+                    <a className="menu-item" href="#">
                         <i><BookmarkIcon htmlColor='blue'/></i>
                         <span className="links-name">Saved</span>
                     </a>
                 </li>
                 <li>
-                    <a href="#">
+                    <a className="menu-item" href="#">
                         <i><SettingsIcon htmlColor='grey'/></i>
                         <span className="links-name">Settings</span>
                     </a>
                 </li>
             </ul>
-            <div className="profile-content">
-                <div className="profile">
-                    <div className="profile-details">
-                        <img src="/assets/person/diana.jpg" alt="" />
-                        <div className="name-job">
-                            <div className="name">Diana Ayi</div>
-                            <div className="job">Front-end Developer</div>
-                        </div>
-                    </div>
-                    <LogoutOutlinedIcon className="log-out"/>
-                </div>
-            </div>
         </div>
     )
 }
