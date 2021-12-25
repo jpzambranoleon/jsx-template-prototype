e8a49c8a63a3b50f03d4c3adeb60ffe8e8e1cb4a
Jean-Paul <jpzambrano.leon@gmail.com>
2021-12-25 02:55:05 -0600

    updated files

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,12 @@
 import Sidebar from "../../components/sidebar/Sidebar";
 import Topbar from "../../components/topbar/Topbar";
-import Header from "../../components/header/Header";
 
 function Home() {
     return (
         <>
             <Topbar />
             <div className="home-container">
+                <Sidebar />
             </div>
         </>
     );
