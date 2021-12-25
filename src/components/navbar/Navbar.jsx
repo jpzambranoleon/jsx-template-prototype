136d8ce38f6ce5f148b6947a1f6af9d5d145e6a9
Jean-Paul <jpzambrano.leon@gmail.com>
2021-12-25 00:17:04 -0600

    updated files

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,25 @@
+import SearchIcon from '@mui/icons-material/Search';
 import "./navbar.css"
 
 function Navbar() {
     return (
         <div className="navbar">
-            <div className="searchbar">
-                <input type="search" placeholder="Search for creators, inspirations, and projects" />
+            <div className="navbar-left">
+                <span className="navbar-link">Dashboard</span>
+            </div>
+            <div className="navbar-center">
+                <div className="searchbar">
+                    <SearchIcon />
+                    <input type="search" placeholder="Search for creators, inspirations, and projects" className="search-input" />
+                </div>
+            </div>
+            <div className="navbar-right">
+                <div className="navbar-profile-photo">
+                    <img src="/assets/person/diana.jpg" alt="" />
+                </div>
+                <span className="username">
+                    <h4>Diana Ayi</h4>
+                </span>
             </div>
         </div>
     )
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,12 @@
-import Navbar from "../../components/navbar/Navbar";
 import Sidebar from "../../components/sidebar/Sidebar";
+import Navbar from "../../components/navbar/Navbar";
 
 function Home() {
     return (
         <>
-            <div>
+            <Navbar />
+            <div className="home-container">
                 <Sidebar />
-                <Navbar />
             </div>
         </>
     );
