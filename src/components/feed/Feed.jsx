ac8bb31f1f4732632cd771146e27d4add04f9828
Jean-Paul <jpzambrano.leon@gmail.com>
2021-12-26 01:14:06 -0600

    updated Feed.jsx

--- src/components/feed/Feed.jsx
+++ src/components/feed/Feed.jsx
@@ -2,49 +2,49 @@ import "./feed.css"
 
 function Feed() {
     return (
-        <div class="feeds">
-            <div class="feed">
-                <div class="head">
-                    <div class="user">
-                        <div class="profile-photo">
+        <div className="feeds">
+            <div className="feed">
+                <div className="head">
+                    <div className="user">
+                        <div className="profile-photo">
                             <img src="/assets/person/diana.jpg" />
                         </div>
-                        <div class="ingo">
+                        <div className="ingo">
                             <h3>Diana Ayi</h3>
                             <small>Dubai, 15 MINUTES AGO</small>
                         </div>
                     </div>
-                    <span class="edit">
-                        <i class="uil uil-ellipsis-h"></i>
+                    <span className="edit">
+                        <i className="uil uil-ellipsis-h"></i>
                     </span>
                 </div>
 
-                <div class="photo">
+                <div className="photo">
                     <img src="./images/post1.jpg" />
                 </div>
 
-                <div class="action-buttons">
-                    <div class="interaction-buttons">
-                        <span><i class="uil uil-heart"></i></span>
-                        <span><i class="uil uil-comment-dots"></i></span>
-                        <span><i class="uil uil-share-alt"></i></span>
+                <div className="action-buttons">
+                    <div className="interaction-buttons">
+                        <span><i className="uil uil-heart"></i></span>
+                        <span><i className="uil uil-comment-dots"></i></span>
+                        <span><i className="uil uil-share-alt"></i></span>
                     </div>
-                    <div class="bookmark">
-                        <span><i class="uil uil-bookmark-full"></i></span>
+                    <div className="bookmark">
+                        <span><i className="uil uil-bookmark-full"></i></span>
                     </div>
                 </div>
 
-                <div class="liked-by">
+                <div className="liked-by">
                     <span><img src="./images/ernest-achiever.jpg" /></span>
                     <span><img src="./images/jane-doe.jpg" /></span>
                     <span><img src="./images/diana-ayi.jpg" /></span>
                     <p>Liked by <b>Ernest Achiever</b> and <b>2, 323 others</b></p>
                 </div>
 
-                <div class="caption">
+                <div className="caption">
                     <p><b>Lana Rose</b> Lorem ipsum dolor sit quisquam eius. <span class="harsh-tag">#lifestyle</span></p>
                 </div>
-                <div class="comments text-muted">View all 277 comments</div>
+                <div className="comments text-muted">View all 277 comments</div>
             </div>
         </div>
     )
