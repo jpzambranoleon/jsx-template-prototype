0f5cc028ea9cb8fd19b96fca6ff85d9f5b4837ce
Jean-Paul <jpzambrano.leon@gmail.com>
2021-12-28 16:02:54 -0600

    updated files

--- src/pages/profile/components/ProfileContainer.jsx
+++ src/pages/profile/components/ProfileContainer.jsx
@@ -2,7 +2,19 @@ import "./profilecontainer.css"
 
 function ProfileContainer() {
     return(
-        <div>Hello boi</div>
+        <div className="profile-container">
+            <div className="profile-cover-photo" style={{backgroundImage: "url(/assets/cover/cover.jpg)"}}>
+                <div className="profile-container-img">
+                    <img src="/assets/person/diana.jpg" />
+                </div>
+            </div>
+            <div className="profile-container-name">
+                Diana Ayi
+                <p className="text-muted">
+                    @dayi
+                </p>
+            </div>
+        </div>
     )
 }
 
