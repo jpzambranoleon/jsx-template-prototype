7fd6b2787c56752437d722fb6711daa28ed1edc7
Jean-Paul <jpzambrano.leon@gmail.com>
2021-12-29 00:51:08 -0600

    moved folders

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,10 @@
 import TopBar from "../../components/topbar/TopBar";
-import Feed from "../../components/feed/Feed";
+import Feed from "./components/feed/Feed";
 import "./home.css"
-import Share from "../../components/share/Share";
+import Share from "./components/share/Share";
 import SideBar from "../../components/sidebar/SideBar";
-import ProfileCard from "../../components/profilecard/ProfileCard";
-import MessageBox from "../../components/messagebox/MessageBox";
+import ProfileCard from "./components/profilecard/ProfileCard";
+import MessageBox from "./components/messagebox/MessageBox";
 
 function Home() {
     return (
