d45eb01e57b031da9210447fc41d53d39717b0eb
Jean-Paul <jpzambrano.leon@gmail.com>
2022-01-12 21:20:10 -0600

    updated ProjectBox.jsx

---
+++ src/pages/home/components/projectbox/ProjectBox.jsx
@@ -0,0 +1,11 @@
+import "./projectbox.css"
+
+function ProjectBox() {
+    return (
+        <div className="project-box-container">
+            Hello boi
+        </div>
+    );
+}
+
+export default ProjectBox;
