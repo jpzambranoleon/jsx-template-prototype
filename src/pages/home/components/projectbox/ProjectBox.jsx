8042fa5368564bd1090a27da09f7c74e57298321
Jean-Paul <jpzambrano.leon@gmail.com>
2022-01-12 23:45:00 -0600

    updated ProjectBox.jsx

--- src/pages/home/components/projectbox/ProjectBox.jsx
+++ src/pages/home/components/projectbox/ProjectBox.jsx
@@ -19,19 +19,8 @@ export default function BasicCard() {
   return (
     <Card sx={{ minWidth: 275 }}>
       <CardContent>
-        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-          Word of the Day
-        </Typography>
-        <Typography variant="h5" component="div">
-          be{bull}nev{bull}o{bull}lent
-        </Typography>
-        <Typography sx={{ mb: 1.5 }} color="text.secondary">
-          adjective
-        </Typography>
-        <Typography variant="body2">
-          well meaning and kindly.
-          <br />
-          {'"a benevolent smile"'}
+        <Typography sx={{fontSize: 14}} variant="h5" component="div">
+          Projects that may interest you
         </Typography>
       </CardContent>
       <CardActions>
