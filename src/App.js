ac07330def17b26e3204612e453185d3743a62c4
Jean-Paul <jpzambrano.leon@gmail.com>
2022-01-13 23:18:35 -0600

    updated App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import Home from './pages/home/Home';
 import ProfilePage from './pages/profile/ProfilePage';
-import {BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return <Home />
