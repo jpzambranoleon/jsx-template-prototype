7a2e404a6f17e8129b982e531c5f7bb8afa5ce75
Jean-Paul <jpzambrano.leon@gmail.com>
2022-01-13 23:09:12 -0600

    updated Home.jsx

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,7 +2,7 @@ import TopBar from "../../components/topbar/TopBar";
 import Feed from "./components/feed/Feed";
 import "./home.css"
 import Share from "./components/share/Share";
-import SideBar from "../../components/sidebar/SideBar";
+import Sidebar from "../../components/sidebar/SideBar";
 import ProfileCard from "./components/profilecard/ProfileCard";
 import MessageBox from "./components/messagebox/MessageBox";
 import ProjectBox from "./components/projectbox/ProjectBox";
