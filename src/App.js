18ca047acf73d0184d14db995176b1f92454fdd9
Jean-Paul <jpzambrano.leon@gmail.com>
2022-01-13 23:27:22 -0600

    updated App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,9 @@ export default function App() {
   return (
     <div className="App">
       <Router>
-        <Router path="/" element={<Home />} />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
       </Router>
     </div>
   )
